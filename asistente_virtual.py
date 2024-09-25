f0a6e905a0f05f941bae7ea5bc64b8b2e18be7c2
SHuertasRo <88412486+SamHuRo@users.noreply.github.com>
2024-09-24 21:08:31 -0500

    update

---
+++ asistente_virtual.py
@@ -0,0 +1,11 @@
+#Librerias a importar
+import pyttsx3
+import speech_recognition as sr
+import pywhatkit
+import yfinance as yf
+import pyjokes
+import webbrowser
+import datetime
+import wikipedia
+
+
